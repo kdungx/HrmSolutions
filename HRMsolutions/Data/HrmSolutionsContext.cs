86470659bcdd7a03a5468b6df3d3aab22c154f4d
kdungx <dungkath2209058@fpt.edu.vn>
2024-04-03 15:44:24 +0700

    Add project files.

---
+++ HRMsolutions/Data/HrmSolutionsContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using HrmSolutions.Models;
+
+namespace HrmSolutions.Data
+{
+    public class HrmSolutionsContext : DbContext
+    {
+        public HrmSolutionsContext (DbContextOptions<HrmSolutionsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<HrmSolutions.Models.Employees> Employees { get; set; } = default!;
+    }
+}
---
+++ HRMsolutions/Models/Employees.cs
@@ -0,0 +1,11 @@
+﻿namespace HrmSolutions.Models
+{
+    public class Employees
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+       
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
